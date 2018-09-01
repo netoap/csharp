b32102b3e163c6db44b669ab9cca875ffb26c3f8
“Alex <alexanderneto5@gmail.com>
2018-09-01 21:29:40 +0100

    Delegate-referência (com type safety) para um ou mais métodos

---
+++ projecto9/Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Delegate.Services;
+
+namespace Delegate
+{
+    // É uma referência (com type safety) para um ou mais métodos
+    // É um tipo referência
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //BinaryNumericOperation op = CalculationService.Sum;
+            //double result = CalculationService.Square(a);
+            BinaryNumericOperation op = CalculationService.Max;
+            double result = op(a,b);
+            Console.WriteLine(result);
+        }
+    }
+}
