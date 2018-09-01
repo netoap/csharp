4a12901bea42c81c78e6e4413eae351feb4e7175
“Alex <alexanderneto5@gmail.com>
2018-09-01 21:20:38 +0100

    Delagate-Intro

---
+++ projecto9/Comparison1/Entities/Product.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+
+namespace Comparison1.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price){
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override string ToString(){
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
