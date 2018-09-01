41c6155973989461b83a8776781706c1bf07851a
“Alex <alexanderneto5@gmail.com>
2018-09-01 21:16:06 +0100

    message

---
+++ projecto1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace projecto1
+{
+    class Program
+    {
+        static void Main(string[] args){   
+            /*
+            int count=0,soma=0, idade =0;
+
+            float media = 0;
+            Console.Write("Idade"+(count+=1)+": ");
+            idade = int.Parse(Console.ReadLine());
+            while(idade >=0){                
+                soma += idade;
+                Console.Write("Idade"+(++count)+": ");
+                idade = int.Parse(Console.ReadLine());                
+            } 
+            count--;
+            if(count>0){
+                media = (float)soma/count;
+                Console.WriteLine("Média = "+ media.ToString("F2",CultureInfo.InvariantCulture)); 
+            }                
+            else{                
+                Console.WriteLine("IMPOSSIVEL CALCULAR"); 
+            }
+             */
+            int N = int.Parse(Console.ReadLine());
+            for(int x =0; x<N; x++){
+                string [] vet = Console.ReadLine().Split(' ');
+                float y = float.Parse(vet[0]);
+                float z = float.Parse(vet[1]);
+                if(z>0){
+                    Console.WriteLine((y/z).ToString("F3",CultureInfo.InvariantCulture));
+                } else{
+                    Console.WriteLine("Não existe");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
