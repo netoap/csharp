6ab0087e68fa8f02aa58f770948b7a3df4372311
“Alex <alexanderneto5@gmail.com>
2018-09-02 22:39:52 +0100

    Demo - LINQ com Lambda - PARTE 1

---
+++ projecto9/LinqComLambda/Entities/Category.cs
@@ -0,0 +1,9 @@
+namespace LinqComLambda.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Tier { get; set; }        
+    }
+}
