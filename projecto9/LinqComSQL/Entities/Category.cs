08ef601985a8f03cb5015016cd3528863459b54c
“Alex <alexanderneto5@gmail.com>
2018-09-03 12:14:49 +0100

    Demo - LINQ com notação similar à SQL

---
+++ projecto9/LinqComSQL/Entities/Category.cs
@@ -0,0 +1,10 @@
+namespace LinqComSQL.Entities
+{
+    public class Category
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Tier { get; set; }  
+    }
+}
